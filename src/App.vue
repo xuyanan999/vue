2cfe96d69358bf733be3f04c4dd93c001b747d45
xuyanan999 <747661339@qq.com>
2019-05-28 16:53:14 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+
+<div class="text">
+      <div id="app">
+        <p>5月28日todo-list</p>
+        <input v-model="text" type="text">
+        <button @click="xyn">提交</button>
+        <ol>
+        <li v-for="(item,index) in list" :key="index" @click="pengrui(index)">{{item}}</li>
+        </ol>
+      </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return ({
+      list:[
+        '联想笔记本',
+        '戴尔笔记本',
+        '华硕笔记本'
+      ],
+      text:''
+    })
+  },
+  methods: {
+    xyn(){
+      this.list.push(this.text),
+      this.text=''
+    },
+    pengrui(index){
+      this.list.splice(index,1)
+    }
+     
+    },
+    components:{
+
+    }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.text{
+  margin:0 auto;
+  margin-top: 200px;
+  width:400px;
+  border:1px solid red;
+  background-color: aquamarine;
 }
+#app{
+  border:1px solid green;
+  text-align: center;
+}
+ol{
+  list-style: none;
+}
+input{
+  background-color:#C1C1C1;
+}
+.bood{
+  background-color: antiquewhite;
+}
+
+
 </style>
